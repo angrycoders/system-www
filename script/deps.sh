7092554d1cbfeed8b41bbbd3fe7aca1aa3db534b
Gocho Mugo <mugo@forfuture.co.ke>
2015-05-26 00:25:00 +0300

    Using new theme Superhero
    A new theme is used: Superhero - https://bootswatch.com/superhero/
    
    Also:
    
    * navigation has been made less cluttered
    * only copyright notice is shown in the footer
    * automated downloading of theme stylesheet

--- script/deps.sh
+++ script/deps.sh
@@ -14,8 +14,11 @@ log "installing node_modules as libraries" 0
 npm install
 mv node_modules/* lib/
 
-log "installing reqwest" 0
 pushd lib
+log "installing reqwest" 0
 wget https://raw.githubusercontent.com/ded/reqwest/master/reqwest.min.js
+log "installing superhero bootswatch theme" 0
+wget https://bootswatch.com/superhero/bootstrap.min.css
+mv bootstrap.min.css superhero.min.css
 popd
 
