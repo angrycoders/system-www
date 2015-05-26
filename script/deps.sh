7a3e7c33bea20818dfc9dcd648b3b345b7e05614
Gocho Mugo <mugo@forfuture.co.ke>
2015-05-26 03:13:55 +0300

    Add angularjs as a dependency
    We shall use AngularJS for a more enhanced web application. See
    https://angularjs.org/ for more information.

--- script/deps.sh
+++ script/deps.sh
@@ -7,20 +7,31 @@ source script/utils.sh
 LOG_TITLE="deps"
 
 
-log "installing composer" 2
-log "installing jekyll" 2
+log "listing installed pre-requisites" 0
+log "node $(node --version)" 1
+log "npm $(npm --version)" 1
+log "$(ruby --version)" 1
+log "$(wget --version | head -n 1 )" 1
 
-log "installing node_modules as libraries" 0
-npm install
-mv node_modules/* lib/
+
+log "installing jekyll" 0
+gem install jekyll
+log "$(jekyll --version)" 1
+
+
+log "installing deps stated in package.json (using npm)" 0
+npm install --production --prefix=lib
+mv lib/node_modules/* lib/
+rm -rf lib/node_modules
+
 
 pushd lib
 log "downloading reqwest (for ajax calls)" 0
-wget https://raw.githubusercontent.com/ded/reqwest/master/reqwest.min.js
+wget -qO- https://raw.githubusercontent.com/ded/reqwest/master/reqwest.min.js
 log "downloading superhero (bootstrap theme)" 0
-wget https://bootswatch.com/superhero/bootstrap.min.css
+wget -qO- https://bootswatch.com/superhero/bootstrap.min.css
 mv bootstrap.min.css superhero.min.css
 log "downloading animate.css (for animations)" 0
-wget https://raw.github.com/daneden/animate.css/master/animate.css
+wget -qO- https://raw.github.com/daneden/animate.css/master/animate.css
 popd
 
